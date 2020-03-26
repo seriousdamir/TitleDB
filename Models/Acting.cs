0c5f6914cc609122f5a853c9227f8069736f3829
panstarrs17 <panstarrs17@gmail.com>
2020-03-26 21:02:03 +0200

    Update Acting.cs
    description

--- Models/Acting.cs
+++ Models/Acting.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TDB
 {
-    public partial class Acting //дароф
+    public partial class Acting //хочу тупицы
     {
         public int TitleId { get; set; }
         public int CharacterId { get; set; }
